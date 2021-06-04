2e278a96485b42f7b1a456db3dbaa1c2ab261470
Brad-Stewart <77695619+Brad-Stewart@users.noreply.github.com>
2021-06-04 14:36:41 -0700

    Add files via upload

---
+++ django_demo.py
@@ -0,0 +1,24 @@
+# # def addTwoThings(a,b):
+# #     c=a+b
+# #     return c
+# # print(addTwoThings(42,56))
+# # print(addTwoThings([1,2] , [5,6]))
+# # print(3)
+# # print(addTwoThings(1,2))  
+
+# def a(b,c):
+#     return b+c
+#     print(b+c)
+# print(a(1,2) + a(2,3))
+
+def a():
+    print(1)
+    x=b()
+    print(x)
+    return 10
+
+def b():
+    print(3)
+    return 5
+y=a()
+print(y)
